ffbcfd4377761a7a16dcb2cb394770320537f815
Aluno SENAI <Aluno@SN-358768.edu.fiesc.com.br>
2026-03-05 11:53:09 -0300

    DELETE 2.0

--- serve.js
+++ serve.js
@@ -32,7 +32,7 @@ servidor.put('/usuarios/:id', async (request, reply) => {
 servidor.delete('/usuarios/:id', async (request, reply) => {
     const id = request.params.id;
     const resultado = await sql.query('delete from usuarios where id = $1', [id]);
-    return 'Usuário Deletado'
+    reply.status(200).send({message: 'Usuário Deletado!'})
 });
 
 servidor.listen({
